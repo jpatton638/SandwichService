195d949038cc282bde65b049e0bbb5497c4ad469
jpatton638 <jpatton638@gmail.com>
2018-04-11 12:18:12 +0100

    Added welcome view

--- test/controllers/WelcomeControllerSpec.scala
+++ test/controllers/WelcomeControllerSpec.scala
@@ -12,13 +12,19 @@ class WelcomeControllerSpec extends PlaySpec with GuiceOneAppPerTest {
   "WelcomeController GET" should {
 
     "return a successful response" in {
-      val result = controller.welcome.apply(FakeRequest())
+      val result = controller.welcome().apply(FakeRequest())
       status(result) mustBe OK
     }
 
     "respond to the /welcome url" in {
-      val result = controller.welcome.apply(FakeRequest())
+      val result = controller.welcome().apply(FakeRequest())
       contentType(result) mustBe Some("text/html")
+    }
+
+    "render a title and h1 correctly" in {
+      val result = controller.welcome().apply(FakeRequest())
+      contentAsString(result) must include ("<title>Welcome</title>")
+      contentAsString(result) must include ("<h1>Hello</h1>")
     }
   }
 }
